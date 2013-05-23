3d6f01c95fe35e037ecd544f93537bf58b336398
alexey <alexey.pankratev@gmail.com>
2013-05-24 02:01:49 +0400

    ok test chapter 5

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,7 +7,7 @@ describe "Static pages" do
 #include Rails.application.routes.url_helpers
   describe "Home page" do
 
-    it "should have the content 'app'" do
+    it "should have the content 'mynda'" do
       visit 'Home'
     # response.should be_redirect  
       response.body.should include("Мянда затерянный край вдали городов !!!") 
@@ -16,11 +16,28 @@ describe "Static pages" do
   
   describe "Help page" do
 
-    it "should have the content 'help'" do
+    it "should have the content 'Primer'" do
       visit 'Help'
     # response.should be_redirect  
       response.body.should include("Пример для саита!") 
     end
   end
 
+  describe "About page" do
+
+    it "should have the content 'Primer'" do
+      visit 'About'
+    # response.should be_redirect  
+      response.body.should include("Пример для саита!") 
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the content 'Zjena'" do
+      visit 'Contact'
+    # response.should be_redirect  
+      response.body.should include("Моя любимая жена!") 
+    end
+  end
 end
