dd314f05baa8638d60c781f69dbce86d9c94c0c4
alexey <alexey.pankratev@gmail.com>
2013-08-05 00:14:48 +0400

    7.3.1 done

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,12 +1,8 @@
-# Используя символ ':user', мы указываем Factory Girl на необходимость симулировать модель User.
-
-Factory.define :user do |user|
-  user.name                  "Michael Hartl"
-  user.email                 "mhartl@example.com"
-  user.password              "foobar"
-  user.password_confirmation "foobar"
-end
-
-Factory.sequence :email do |n|
-  "person-#{n}@example.com"
+FactoryGirl.define do
+  factory :user do
+    name     "Michael Hartl"
+    email    "michael@example.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -11,65 +11,73 @@ describe "Users" do
     it { should have_selector('title', content: full_title('Sign up')) }
   end
 
- describe "signup" do
-  describe "failure" do
+   describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
 
-   it "should not make a new user" do
-    lambda do
-       visit signup_path
-        fill_in "Name",         :with => ""
-        fill_in "Email",        :with => ""
-        fill_in "Password",     :with => ""
-        fill_in "Confirmation", :with => ""
-        click_button
-        response.should render_template('users/new')
-        response.should have_selector("div#error_explanation")
-    end.should_not change(User, :count)
-   end
-  end
-  
-  describe "success" do
-   it "should make a new user" do
-    lambda do
-          visit signup_path
-          fill_in "Name",         :with => "Example User"
-          fill_in "Email",        :with => "user@example.com"
-          fill_in "Password",     :with => "foobar"
-          fill_in "Confirmation", :with => "foobar"
-          click_button
-          response.should have_selector("div.flash.success",
-                                        :content => "Welcome")
-          response.should render_template('users/show')
-    end.should change(User, :count).by(1)
-   end
-  end
- end
-
- describe "sign in/out" do
-
-    describe "failure" do
-      it "should not sign a user in" do
-        visit signin_path
-        fill_in :email,    :with => ""
-        fill_in :password, :with => ""
-        click_button
-        response.should have_selector("div.flash.error", :content => "Invalid")
-      end
-    end
-
-    describe "success" do
-      it "should sign a user in and out" do
-        #user = Factory(:user)
-        #visit signin_path
-        #fill_in :email,    :with => user.email
-        #fill_in :password, :with => user.password
-        #click_button
-        #integration_sign_in(Factory(:user))
-        #controller.should be_signed_in
-        #click_link ("Войти!")
-        #controller.should_not be_signed_in
-      end
-    end
+    it { should have_selector('h1',    content: user.name) }
+    it { should have_selector('title', content: user.name) }
   end
 end
+#  describe "signup" do
+#   describe "failure" do
+
+#    it "should not make a new user" do
+#     lambda do
+#        visit signup_path
+#         fill_in "Name",         :with => ""
+#         fill_in "Email",        :with => ""
+#         fill_in "Password",     :with => ""
+#         fill_in "Confirmation", :with => ""
+#         click_button
+#         response.should render_template('users/new')
+#         response.should have_selector("div#error_explanation")
+#     end.should_not change(User, :count)
+#    end
+#   end
+  
+#   describe "success" do
+#    it "should make a new user" do
+#     lambda do
+#           visit signup_path
+#           fill_in "Name",         :with => "Example User"
+#           fill_in "Email",        :with => "user@example.com"
+#           fill_in "Password",     :with => "foobar"
+#           fill_in "Confirmation", :with => "foobar"
+#           click_button
+#           response.should have_selector("div.flash.success",
+#                                         :content => "Welcome")
+#           response.should render_template('users/show')
+#     end.should change(User, :count).by(1)
+#    end
+#   end
+#  end
+
+#  describe "sign in/out" do
+
+#     describe "failure" do
+#       it "should not sign a user in" do
+#         visit signin_path
+#         fill_in :email,    :with => ""
+#         fill_in :password, :with => ""
+#         click_button
+#         response.should have_selector("div.flash.error", :content => "Invalid")
+#       end
+#     end
+
+#     describe "success" do
+#       it "should sign a user in and out" do
+#         #user = Factory(:user)
+#         #visit signin_path
+#         #fill_in :email,    :with => user.email
+#         #fill_in :password, :with => user.password
+#         #click_button
+#         #integration_sign_in(Factory(:user))
+#         #controller.should be_signed_in
+#         #click_link ("Войти!")
+#         #controller.should_not be_signed_in
+#       end
+#     end
+#   end
+# end
 
