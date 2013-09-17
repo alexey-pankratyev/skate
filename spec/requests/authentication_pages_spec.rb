962d5f61ed929ad0535364a97591f109ec2b25e0
alexey <alexey.pankratev@gmail.com>
2013-09-18 00:20:14 +0400

    start updating-users9

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -9,7 +9,10 @@ describe "Authentication" do
 
   describe "signin page" do
     before { visit signin_path }  
-    it { should have_content_h1_title('Sign in') }
+
+    it { should have_selector('h1',    content: 'Sign in') }
+    it { should have_selector('title', content: 'Sign in') }
+
   end
 
   describe "signin" do
@@ -19,32 +22,40 @@ describe "Authentication" do
     describe "with invalid information" do
       before { click_button "Sign in" }
 
-      it { should have_content_h1_title('Sign in') }
-      it { response.body.should have_error_message('Invalid') }
+      it { should have_selector('title', content: 'Sign in') }
+      it { should have_selector('div.alert.alert-error', content: 'Invalid') }
 
       describe "after visiting another page" do
         it { response.body.should have_link('Главная', :href => root_path) }
         before { click_link "Главная" }
-        it { should_not have_selector('Invalid') }
-
+        it { should_not have_selector('div.alert.alert-error') }
       end
 
     end
   
+
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
       
-      before { valid_signin(user) }
+      before do
+        visit signin_path
+        fill_in "Email",    with: user.email.upcase
+        fill_in "Password", with: user.password
+        click_button "Sign in"
+      end 
       
-      it { should have_content_h1_title(user.name) }
+      it { should have_selector('title', content: user.name) }
+      it { response.body.should have_link('Настройка', href: edit_user_path(user)) }
       it { response.body.should have_link('Профиль', href: user_path(user)) }
       it { response.body.should have_link('Выйти', href: signout_path) }
       
       describe "followed by signout" do
          before { click_link "Выйти", :method => :delete }
          it { response.body.should have_link('Войти', href: signin_path) }
+         it { response.body.should_not have_link('Настройка', href: edit_user_path(user)) }
          it { response.body.should_not have_link('Профиль', href: user_path(user)) }
          it { response.body.should_not have_link('Выйти', href: signout_path) }
+
       end
       
     end
