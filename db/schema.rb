12d0d4be7765ec8123f67e5f7cee8fad529a1b75
alexey <alexey.pankratev@gmail.com>
2013-12-04 23:16:39 +0400

    Add user following

--- db/schema.rb
+++ db/schema.rb
@@ -51,7 +51,7 @@ ActiveRecord::Schema.define(:version => 20131124185944) do
     t.string   "remember_token"
     t.string   "nickname"
   end
-
+ 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["nickname"], :name => "index_users_on_nickname", :unique => true
   add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
