bdab9f90a97867b4594bf162f57360a0dd1cc601
alexey <alexey.pankratev@gmail.com>
2014-05-24 07:33:41 +0400

    add sass,add paginate, edit views - modeling-directmessages

--- app/controllers/direct_messages_controller.rb
+++ app/controllers/direct_messages_controller.rb
@@ -3,22 +3,18 @@ class DirectMessagesController < ApplicationController
  
   before_filter :signed_in_user
 
-
-     
-
   def received
-  	   
-     @direct_messages = DirectMessage.where(["recipient_id = ?", current_user])
+
+  	@user= DirectMessage.where(["recipient_id = ?", current_user]) 
+    @direct_messages = @user.paginate(page: params[:page])
+     
       
   end
 
   def sent
 
-    @direct_messages = DirectMessage.where(["sender_id = ?", current_user])
-
+    @user = DirectMessage.where(["sender_id = ?", current_user])
+    @direct_messages = @user.paginate(page: params[:page])
   end
-
-  
-
 
 end
