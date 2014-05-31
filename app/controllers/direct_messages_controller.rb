ab2913efcdab3d1564eeb9cb4adc7a3d17f4d8f0
alexey <alexey.pankratev@gmail.com>
2014-05-31 17:02:05 +0400

    added template-'direct_message'

--- app/controllers/direct_messages_controller.rb
+++ app/controllers/direct_messages_controller.rb
@@ -1,17 +1,17 @@
 class DirectMessagesController < ApplicationController
   
-  attr_accessor :use, :recipient, :sender 
   before_filter :signed_in_user
 
   def received
   	@user= DirectMessage.where(["recipient_id = ?", current_user]) 
     @direct_messages = @user.paginate(page: params[:page])
-    @use="sender"
+    @use='sender'
   end
 
   def sent
     @user = DirectMessage.where(["sender_id = ?", current_user])
     @direct_messages = @user.paginate(page: params[:page])
+    @use='recipient'
   end
 
 end
