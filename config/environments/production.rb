db357b1f96f8f3c0d76756e38a6a75ea559a954c
alexey <alexey.pankratev@gmail.com>
2014-08-01 19:27:04 +0400

    --amend

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ Myndozero::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  #config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
@@ -29,7 +29,7 @@ Myndozero::Application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  # config.force_ssl = true
 
   # See everything in the log (default is :info)
   # config.log_level = :debug
@@ -66,4 +66,5 @@ Myndozero::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
 end
