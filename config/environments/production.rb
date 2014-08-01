9b96dfb8e2592a607d6593c1c300fe17d113e526
alexey <alexey.pankratev@gmail.com>
2014-08-01 19:55:22 +0400

    --amend

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,19 +60,10 @@ Myndozero::Application.configure do
   config.i18n.fallbacks = true
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :silence #:notify
- 
-  #mailers
- config.action_mailer.delivery_method = :smtp
- config.action_mailer.smtp_settings = {
-  address:              'smtp.gmail.com',
-  port:                 587,
-  domain:               'alexey.pankratev@gmail.com',
-  user_name:            'alexey.pankratev',
-  authentication:       'plain',
-  enable_starttls_auto: true ,
-  password: ENV['SMTP_PASSWORD'],
- }
- config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: "localhost:80" }
+
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
